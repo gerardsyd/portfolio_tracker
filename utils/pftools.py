be69cd3153fd6396b25a16c077674d8f493ae6d5
gerardsyd <62287799+gerardsyd@users.noreply.github.com>
2020-09-13 22:01:46 +1000

    added helper methods to view / edit data

---
+++ utils/pftools.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+def csv_to_pkl(csv_file_path: str, pkl_file_path: str):
+    df = pd.read_csv(csv_file_path, parse_dates=['Date'], dayfirst=True)
+    df.to_pickle(pkl_file_path)
+    return df
+
+
+def pkl_to_csv(csv_file_path: str, pkl_file_path: str):
+    df = pd.read_pickle(pkl_file_path)
+    df.to_csv(csv_file_path, index=False)
+    return df
+
+
+if __name__ == "__main__":
+    # df = pkl_to_csv('data/pf_data_NEW.csv', 'data/pf_data.pkl')
+    # df = csv_to_pkl('data/pf_trades_NEW.csv', 'data/pf_trades.pkl')
+    # print(df)
+
+    # df = pd.read_pickle('data/pf_trades.pkl')
+    # df.drop(columns=['index'], inplace=True)
+    # df.reset_index(inplace=True, drop=True)
+    # df.to_pickle('data/pf_trades.pkl')
+    # print(df)
+    pass
