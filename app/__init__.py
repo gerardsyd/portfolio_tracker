5326ffcafa5455a7543d5938bea604c52e698c83
Gerard-Laptop <gerard.pais@gmail.com>
2023-02-28 16:02:31 +1100

    updated logging to include stdout and to file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 import logging
-import os
+import sys
 
 from flask import Flask
 from flask_login import LoginManager
@@ -8,8 +8,13 @@ from flask_migrate import Migrate
 
 from config import Config
 
+# set up logging
+file_handler = logging.FileHandler(filename=r'logs/logs.log')
+stdout_handler = logging.StreamHandler(sys.stdout)
+handlers = [file_handler, stdout_handler]
 logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
-                    datefmt='%d-%m-%Y:%H:%M:%S', level=logging.DEBUG, filename=r'logs/logs.log')
+                    datefmt='%d-%m-%Y:%H:%M:%S', level=logging.INFO, handlers=handlers)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 app.config.from_object(Config)
